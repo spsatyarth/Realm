93be3668ee34a1069ab41f258edfd21e2f90e79b
Satyarth Kumar Prasad <satyarth.prasad@studypadinc.com>
2015-10-01 15:58:22 +0530

    initial commit

---
+++ RealmTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmTest
+//
+//  Created by Satyarth on 01/10/2015.
+//  Copyright (c) 2015 Satyarth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
